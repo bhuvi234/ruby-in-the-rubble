1f491441d2327b4b8a1585ceed104e3a5b23598b
root <root@201011JUNC0397.(none)>
2015-03-05 14:58:35 +0530

    added string concatination

---
+++ my_first_program.rb
@@ -0,0 +1,13 @@
+puts 2+4
+puts 3+2
+puts 4/2
+puts 2%2
+print 2+2
+print 2+1
+print "\n"
+gets
+first_name="bhuvi"
+last_name="visu"
+favorite_color="blue"
+
+puts "hi,my name is" +first_name+".my fav color is" +favorite_color
